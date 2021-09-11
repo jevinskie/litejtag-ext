91a3e8cb875f9e5a3a824155824fa151dc11b7bd
Jevin Sweval <jevinsweval@gmail.com>
2021-09-11 14:45:58 -0700

    finally works with litex fixes

--- examples/max10_dev_kit.py
+++ examples/max10_dev_kit.py
@@ -24,7 +24,7 @@ from litex.soc.cores.jtag import JTAGPHY, MAX10JTAG, JTAGTAPFSM
 # Bench SoC ----------------------------------------------------------------------------------------
 
 class BenchSoC(SoCCore):
-    def __init__(self, with_jtagbone=False, sys_clk_freq=int(200e6)):
+    def __init__(self, with_jtagbone=False, with_analyzer=False, sys_clk_freq=int(100e6)):
         platform = altera_max10_dev_kit.Platform()
 
         # SoCMini ----------------------------------------------------------------------------------
@@ -53,33 +53,38 @@ class BenchSoC(SoCCore):
             self.hello_tdo = hello_tdo = Signal()
             self.submodules.jtag_hello = JTAGHello(self.jtag_phy.tmsutap, self.jtag_phy.tckutap, self.jtag_phy.tdiutap,
                                                    hello_tdo, ResetSignal("sys"), self.jtag_phy)
-            self.jtag_phy.tdo.eq(hello_tdo)
+            self.comb += self.jtag_phy.tdo.eq(hello_tdo)
 
         # UARTBone ---------------------------------------------------------------------------------
         self.add_uartbone(baudrate=3_000_000)
 
         # scope ------------------------------------------------------------------------------------
-        from litescope import LiteScopeAnalyzer
+        if with_analyzer:
+            from litescope import LiteScopeAnalyzer
 
-        # phy_sigs = self.jtag_phy._signals
-        # phy_sigs.remove(self.jtag_phy.altera_reserved_tdo) # wont pass fitter, output must go to pin
-        # phy_sigs.remove(self.jtag_phy.tdocore)
-        self.jtag_phy.do_finalize()
-        phy_sigs = self.jtag_phy._signals_recursive
-        phy_sigs.remove(self.jtag_phy.altera_reserved_tdo) # wont pass fitter, output must go to pin
-        phy_sigs.remove(self.jtag_phy.tdocore)
+            if with_analyzer:
+                jtag_phy = self.jtagbone_phy.jtag
+            else:
+                jtag_phy = self.jtag_phy
+            jtag_phy.do_finalize()
+            jtag_phy_sigs = jtag_phy._signals_recursive
+            jtag_phy_sigs.remove(jtag_phy.altera_reserved_tdo) # wont pass fitter, output must go to pin
+            jtag_phy_sigs.remove(jtag_phy.tdocore)
 
-        hello_sigs = self.jtag_hello._signals_recursive
+            if hasattr(self, 'jtag_hello'):
+                hello_sigs = self.jtag_hello._signals_recursive
+            else:
+                hello_sigs = []
 
-        analyzer_signals = [
-            *phy_sigs,
-            *hello_sigs,
-        ]
-        self.submodules.analyzer = LiteScopeAnalyzer(analyzer_signals,
-                                                     depth=512,
-                                                     clock_domain="sys",
-                                                     register=True,
-                                                     csr_csv="analyzer.csv")
+            analyzer_signals = [
+                *jtag_phy_sigs,
+                *hello_sigs,
+            ]
+            self.submodules.analyzer = LiteScopeAnalyzer(analyzer_signals,
+                                                         depth=512,
+                                                         clock_domain="sys",
+                                                         register=True,
+                                                         csr_csv="analyzer.csv")
 
         # LEDs -------------------------------------------------------------------------------------
         from litex.soc.cores.led import LedChaser
@@ -94,9 +99,10 @@ def main():
     parser.add_argument("--build",       action="store_true", help="Build bitstream")
     parser.add_argument("--load",        action="store_true", help="Load bitstream")
     parser.add_argument("--with-jtagbone", action="store_true", help="Enable Jtagbone support")
+    parser.add_argument("--with-analyzer", action="store_true", help="Enable litescope")
     args = parser.parse_args()
 
-    soc     = BenchSoC(with_jtagbone=args.with_jtagbone)
+    soc     = BenchSoC(with_jtagbone=args.with_jtagbone, with_analyzer=args.with_analyzer)
     builder = Builder(soc, csr_csv="csr.csv")
     builder.build(run=args.build)
 
